b5be2cba65bc83300fb95f0f3da0dfffd4a98100
Rita Radovics <Rita@ip-10-110-180-30.ec2.internal>
2018-06-08 15:15:02 +0200

    bar info left upper corner done

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ let data = FooBar.getData();
 
 //transfer data to JSON
 const jsondata = JSON.parse(data);
-console.log(jsondata);
+
+function show() {
+    console.log(jsondata);
+
+    document.querySelector("#barname").textContent = jsondata.bar.name;
+    document.querySelector("#closing").textContent = jsondata.bar.closingTime;
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    document.querySelector("#currenttime").textContent = hours + ":" + minutes;
+  
+
+};
+
+show();
