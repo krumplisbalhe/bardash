db8000b1c3b32e3672664bd70d859c41b64ece64
Rita Radovics <Rita@Ritas-MacBook-Pro-2.local>
2018-06-13 15:19:34 +0200

    Wednesday afternoon

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function loadJson() {
         color: "#808080",
         scale: 0.9
     });
-    document.querySelector("#closing").textContent = "Closing time: " + jsondata.bar.closingTime;
+    document.querySelector("#closing").textContent = "Closing time: " + jsondata.bar.closingTime.slice(0, -3);
     //showing current time
     const now = new Date();
     const hours = now.getHours();
@@ -217,7 +217,7 @@ function loadJson() {
          
                 if (tap.beer == beertype.name){
 
-                    clone.querySelector(".alcohol").textContent = beertype.alc;
+                    clone.querySelector(".alcohol").textContent = "Alc.: " + beertype.alc + "%";
                     clone.querySelector(".beercardpicture").src = "labelimages/" + beertype.label;
 
                 }
@@ -227,7 +227,7 @@ function loadJson() {
 
 
             document.querySelector(".beerinfo").appendChild(clone);
-            
+
             // When the user clicks anywhere outside of the modal, close it
             window.onclick = function(event) {
                 if (event.target == modal) {
@@ -263,4 +263,4 @@ document.querySelector(".close").addEventListener("click", closeModal);
 
     setInterval(
         loadJson,
-        10000);
+        100000);
